6c4748d9950fd4ef414cb6d52d465c8a775ffce8
Muhammed Rinshad <mrmuhammedrinshad@gmail.com>
2026-07-22 15:12:51 +0530

    Update activitylifecycle.java

--- ActivityLifeCycle/activitylifecycle.java
+++ ActivityLifeCycle/activitylifecycle.java
@@ -1,61 +1,54 @@
-package com.example.activitylifecycle;
+package com.example.activity;
 
 import android.os.Bundle;
-import android.util.Log;
+import android.widget.Toast;
 
+import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-
-    String TAG = "ActivityLifeCycle";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Log.d(TAG, "onCreate() called");
+        Toast.makeText(this, "onCreate()", Toast.LENGTH_SHORT).show();
     }
 
     @Override
     protected void onStart() {
         super.onStart();
-
-        Log.d(TAG, "onStart() called");
+        Toast.makeText(this, "onStart()", Toast.LENGTH_SHORT).show();
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-
-        Log.d(TAG, "onResume() called");
+        Toast.makeText(this, "onResume()", Toast.LENGTH_SHORT).show();
     }
 
     @Override
     protected void onPause() {
         super.onPause();
-
-        Log.d(TAG, "onPause() called");
+        Toast.makeText(this, "onPause()", Toast.LENGTH_SHORT).show();
     }
 
     @Override
     protected void onStop() {
         super.onStop();
-
-        Log.d(TAG, "onStop() called");
+        Toast.makeText(this, "onStop()", Toast.LENGTH_SHORT).show();
     }
 
     @Override
     protected void onRestart() {
         super.onRestart();
-
-        Log.d(TAG, "onRestart() called");
+        Toast.makeText(this, "onRestart()", Toast.LENGTH_SHORT).show();
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
-
-        Log.d(TAG, "onDestroy() called");
+        Toast.makeText(this, "onDestroy()", Toast.LENGTH_SHORT).show();
     }
 }
