dbb2c0d65c3d2c06c7e37d0fa401b2d735d4d031
Muhammed Rinshad <mrmuhammedrinshad@gmail.com>
2026-08-19 12:34:23 +0530

    Delete saveddata.java

--- registration/saveddata.java
+++
@@ -1,6 +0,0 @@
-SharedPreferences sp = getSharedPreferences(
-        "UserData", MODE_PRIVATE);
-
-String name = sp.getString("name", "");
-String email = sp.getString("email", "");
-String password = sp.getString("password", "");
