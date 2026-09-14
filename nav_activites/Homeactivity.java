8fe88546e99d7df27434925f7933d9af3a737c42
Muhammed Rinshad <mrmuhammedrinshad@gmail.com>
2026-09-14 14:42:51 +0530

    Update Homeactivity.java

--- nav_activites/Homeactivity.java
+++ nav_activites/Homeactivity.java
@@ -1,7 +1,9 @@
-package com.example.optionmenu;
+package com.example.optionmenuapp;
 
+import android.content.Intent;
 import android.os.Bundle;
-
+import android.view.Menu;
+import android.view.MenuItem;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class HomeActivity extends AppCompatActivity {
@@ -10,5 +12,29 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.menu_home) {
+            // Already in HomeActivity
+            return true;
+        }
+
+        if (id == R.id.menu_about) {
+            Intent intent = new Intent(HomeActivity.this, AboutActivity.class);
+            startActivity(intent);
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
