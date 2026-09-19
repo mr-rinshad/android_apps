99c6d7d82748cbc3cca91c8ae19b4280b09235e9
Muhammed Rinshad <mrmuhammedrinshad@gmail.com>
2026-09-19 15:48:04 +0530

    Update mainactivity.java

--- listview/mainactivity.java
+++ listview/mainactivity.java
@@ -1,6 +1,9 @@
 package com.example.arrayadapterlistview;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -8,32 +11,48 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
-    ListView listView;
-
-    String[] students = {
-            "Rinshad",
-            "Aqif",
-            "Rishal",
-            "Misba",
-            "Fidha",
-            "Anjum",
-            "Nidha",
-            "Rumaisa"
+    String[] fruits = {
+            "apple",
+            "orange",
+            "banana",
+            "pomegranate",
+            "dates"
     };
+
+    ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
 
-        listView = findViewById(R.id.listView);
+        listView = findViewById(R.id.listview);
 
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(
                 this,
                 android.R.layout.simple_list_item_1,
-                students
+                fruits
         );
 
-        listView.setAdapter(adapter);
+        listView.setAdapter(arrayAdapter);
+
+        listView.setOnItemClickListener(
+                new AdapterView.OnItemClickListener() {
+
+                    @Override
+                    public void onItemClick(
+                            AdapterView<?> adapterView,
+                            View view,
+                            int position,
+                            long id) {
+
+                        Log.i(
+                                "ListView",
+                                "Item is clicked @position " + position
+                        );
+                    }
+                }
+        );
     }
 }
